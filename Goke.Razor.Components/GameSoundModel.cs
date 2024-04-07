4c50bd18653dc07fdbedebf1f9ed4d2005c787e4
goke Oladokun <gokeladokun@gmail.com>
2024-04-07 14:45:24 +0100

    AB#326, AB#327 Added Goke.Razor.Components and GameSound

---
+++ Goke.Razor.Components/GameSoundModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Goke.Razor.Components
+{
+    public enum GameStatus { None, Success, Fail, Start, End }
+
+    public class GameSoundModel
+    {
+    }
+}
