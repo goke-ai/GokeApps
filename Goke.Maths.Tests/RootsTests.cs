7d7f09af52877f0949dc92e3a85e92979b175c3d
goke Oladokun <gokeladokun@gmail.com>
2024-10-01 08:47:24 +0100

    Fix Books after OnTranslation

---
+++ Goke.Maths.Tests/RootsTests.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Goke.Maths;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Goke.Maths.Tests
+{
+    [TestClass()]
+    public class RootsTests
+    {
+        [TestMethod()]
+        [DataRow(1.0, -3.0, 2.0, new double[] { 1, 2, 0,0 })]
+        public void FormulaTest(double a, double b, double c, double[] expected)
+        {
+            var actual = Roots.Formula(a,b,c);
+
+            Assert.AreEqual(expected[0], actual.r1);
+            Assert.AreEqual(expected[1], actual.r2);
+            Assert.AreEqual(expected[2], actual.i1);
+            Assert.AreEqual(expected[3], actual.i2);
+        }
+    }
+}
--- Goke.Maths/Roots.cs
+++ Goke.Maths/Roots.cs
@@ -8,5 +8,49 @@ namespace Goke.Maths
 {
     public class Roots
     {
+        /// <summary>
+        /// Method <c>Formula</c> Retuns quadratic equation roots (real and imaginary)
+        /// </summary>
+        /// <param name="a">coefficient of x^2</param>
+        /// <param name="b"></param>
+        /// <param name="c"></param>
+        /// <returns>
+        /// <para>r1- real root 1</para>    
+        /// <para>r2: real root 2</para> 
+        /// <para>i1- imagnary root 1</para> 
+        /// <para>i2: imagnary root 2</para> 
+        /// </returns>
+        /// <exception cref="ArgumentException"></exception>
+        public static (double r1, double r2,double i1,double i2) Formula(double a, double b, double c)
+        {
+            double r1 = 0, r2 = 0, i1 = 0, i2= 0;
+            if (a == 0)
+            {
+                if (b != 0)
+                { 
+                    r1 = -c/b;
+                }
+                else
+                {
+                    throw new ArgumentException("Trivial solution.");
+                }
+            }
+            else
+            {
+                var D = Math.Pow(b, 2) - 4 * a * c;
+                if (D <= 0)
+                {
+                    r1 = -b / 2 / a;
+                    r2 = r1;
+                    i1 = Math.Sqrt(Math.Abs(D)) / 2 / a;
+                    i2 = -i1;
+                    // throw new ArithmeticException("Complex root expected");
+                }
+
+                r1 = (-b - Math.Sqrt(D)) / 2 / a;
+                r2 = (-b + Math.Sqrt(D)) / 2 / a;
+            }
+            return (r1, r2, i1, i2);
+        }
     }
 }
